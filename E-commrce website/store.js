d6efd4d840ceec695229d861e8b0ec0e4269fa96
Lalit Rajak <lalit92rajak@gmail.com>
2022-08-06 01:47:25 +0530

    task11

--- E-commrce website/store.js
+++ E-commrce website/store.js
@@ -1,49 +1,70 @@
 const cart_items = document.querySelector('#cart .cart-items');
 
 
-const parentContainer = document.getElementById('EcommerceContainer');
-parentContainer.addEventListener('click',(e)=>{
+const parentNode = document.getElementById('music-content');
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    console.log('data');
+
+    axios.get('http://localhost:3000/products').then((data) => {
+        console.log(data);
+        if(data.request.status === 200) {
+            const products = data.data.products;
+            const parentSection = document.getElementById('Products');
+            products.forEach(product => {
+                const productHtml = `
+                <div>
+                    <h1>${product.title}</h1>
+                    <img src=${product.imageUrl}></img>
+                    <button> Add To Cart </button>
+
+                </div>`
+                parentSection.innerHTML += productHtml;
+            })
+        }
+        // products.data.forEach(product => {
+        //     const productHtml = `
+        //         <div id="album-${product.id}">
+        //             <h3>${product.title}</h3>
+        //             <div class="image-container">
+        //                 <img class="prod-images" src=${product.imageUrl} alt="">
+        //             </div>
+        //                             <div class="prod-details">
+        //                 <span>$<span>${product.price}</span></span>
+        //                 <button class="shop-item-button" type='button'>ADD TO CART</button>
+        //             </div>
+        //         </div>`
+        //     parentNode.innerHTML += productHtml
+
+        // })
+    })
+
+})
+
+
+document.addEventListener('click',(e)=>{
 
     if (e.target.className=='shop-item-button'){
-        const id = e.target.parentNode.parentNode.id
-        const name = document.querySelector(`#${id} h3`).innerText;
-        const img_src = document.querySelector(`#${id} img`).src;
-        const price = e.target.parentNode.firstElementChild.firstElementChild.innerText;
-        let total_cart_price = document.querySelector('#total-value').innerText;
-        if (document.querySelector(`#in-cart-${id}`)){
-            alert('This item is already added to the cart');
-            return
-        }
-        document.querySelector('.cart-number').innerText = parseInt(document.querySelector('.cart-number').innerText)+1
-        const cart_item = document.createElement('div');
-        cart_item.classList.add('cart-row');
-        cart_item.setAttribute('id',`in-cart-${id}`);
-        total_cart_price = parseFloat(total_cart_price) + parseFloat(price)
-        total_cart_price = total_cart_price.toFixed(2)
-        document.querySelector('#total-value').innerText = `${total_cart_price}`;
-        cart_item.innerHTML = `
-        <span class='cart-item cart-column'>
-        <img class='cart-img' src="${img_src}" alt="">
-            <span>${name}</span>
-    </span>
-    <span class='cart-price cart-column'>${price}</span>
-    <span class='cart-quantity cart-column'>
-        <input type="text" value="1">
-        <button>REMOVE</button>
-    </span>`
-        cart_items.appendChild(cart_item)
+        const prodId = Number(e.target.parentNode.parentNode.id.split('-')[1]);
+        axios.post('http://localhost:3000/cart', { productId: prodId}).then(data => {
+            if(data.data.error){
+                throw new Error('Unable to add product');
+            }
+            showNotification(data.data.message, false);
+        })
+        .catch(err => {
+            console.log(err);
+            showNotification(err, true);
+        });
 
-        const container = document.getElementById('container');
-        const notification = document.createElement('div');
-        notification.classList.add('notification');
-        notification.innerHTML = `<h4>Your Product : <span>${name}</span> is added to the cart<h4>`;
-        container.appendChild(notification);
-        setTimeout(()=>{
-            notification.remove();
-        },2500)
     }
     if (e.target.className=='cart-btn-bottom' || e.target.className=='cart-bottom' || e.target.className=='cart-holder'){
-        document.querySelector('#cart').style = "display:block;"
+        axios.get('http://localhost:3000/cart').then(carProducts => {
+            showProductsInCart(carProducts.data);
+            document.querySelector('#cart').style = "display:block;"
+
+        })
     }
     if (e.target.className=='cancel'){
         document.querySelector('#cart').style = "display:none;"
@@ -53,18 +74,7 @@ parentContainer.addEventListener('click',(e)=>{
             alert('You have Nothing in Cart , Add some products to purchase !');
             return
         }
-        alert('Thanks for the purchase')
-        cart_items.innerHTML = ""
-        document.querySelector('.cart-number').innerText = 0
-        document.querySelector('#total-value').innerText = `0`;
-    }
-
-    if (e.target.innerText=='REMOVE'){
-        let total_cart_price = document.querySelector('#total-value').innerText;
-        total_cart_price = parseFloat(total_cart_price).toFixed(2) - parseFloat(document.querySelector(`#${e.target.parentNode.parentNode.id} .cart-price`).innerText).toFixed(2) ;
-        document.querySelector('.cart-number').innerText = parseInt(document.querySelector('.cart-number').innerText)-1
-        document.querySelector('#total-value').innerText = `${total_cart_price.toFixed(2)}`
-        e.target.parentNode.parentNode.remove()
+        alert('This Feature is yet to be completed ')
     }
 })
 
@@ -92,3 +102,27 @@ function showProductsInCart(listofproducts){
         cart_items.appendChild(cart_item)
     })
 }
+function deleteCartItem(e, prodId){
+    e.preventDefault();
+    axios.post('http://localhost:3000/cart-delete-item', {productId: prodId})
+        .then(() => removeElementFromCartDom(prodId))
+}
+
+function showNotification(message, iserror){
+    const container = document.getElementById('container');
+    const notification = document.createElement('div');
+    notification.style.backgroundColor = iserror ? 'red' : 'green';
+    notification.classList.add('notification');
+    notification.innerHTML = `<h4>${message}<h4>`;
+    container.appendChild(notification);
+    setTimeout(()=>{
+        notification.remove();
+    },2500)
+}
+
+function removeElementFromCartDom(prodId){
+        document.getElementById(`in-cart-album-${prodId}`).remove();
+        showNotification('Succesfuly removed product')
+}
+
+  
